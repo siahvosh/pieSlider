bbdaa868f79d4889094adac4281e23f53920b29d
siahvosh <siablack370@gmail.com>
2024-01-27 19:59:47 +0330

    first cm

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+
+        <div class="main"></div>
+
+
+
+</template>
+
+<script setup>
+  //
+</script>
+
+<style>
+body{
+
+}
+
+
+.main{
+  width: 100%;
+  height: 100%;
+  background: red !important;
+}
+
+</style>
