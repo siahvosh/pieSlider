5eeecb66f0173636bb933bab6fcc0131bd6f9b18
siahvosh <siablack370@gmail.com>
2024-01-29 21:23:46 +0330

    cols

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,9 +5,9 @@
 
           <v-row cols="12" style="height: 40%" justify="center" align-content="center">
             <v-col cols="4" style="border: 1px solid white; height: 100%">
-              <v-row>
-                <img src="" alt="">
-              </v-row>
+
+                <img class="stra-img-1" src="file:///C:/Users/PC/Desktop/%E2%80%94Pngtree%E2%80%94a%20strawberry_4435823.png" alt="">
+
 
             </v-col>
             <v-col cols="4" style="border: 1px solid white; align-self: center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, iure?</v-col>
@@ -36,6 +36,25 @@ body{
   width: 100%;
   height: 100%;
   background: #FF7B54 !important;
+}
+
+.stra-img-1{
+  width: 200px;
+  position: absolute;
+  display: flex;
+  filter: drop-shadow(5px 0 5px black);
+  transform: translate(-20px, -20px);
+  animation: rotate 1s ease 0.2s 1;
+  transform-style: preserve-3d;
+}
+@keyframes rotate {
+  from{
+    rotate: 0;
+  }
+  to{
+    rotate: 360deg;
+  }
+
 }
 
 
