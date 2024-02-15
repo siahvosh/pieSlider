88b45b6f1b2ac36fa6fbe7869cde3cd27adeec37
siahvosh <siablack370@gmail.com>
2024-02-16 01:12:48 +0330

    label after

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -42,9 +42,10 @@
         <svg style="transform: translate(5px, 4px)" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2s-.9-2-2-2M1 4h2l3.6 7.59L3.62 17H19v-2H7l1.1-2h8.64l4.97-9H5.21l-.94-2H1zm16 14c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2s2-.9 2-2s-.9-2-2-2"/></svg>
       </button>
     </div>
-    <div style="position: absolute; top: 30px; left: 30px">
-      <img width="70" height="90"  src="file:///C:/Users/PC/Desktop/1x/7797864.png" alt="">
-      <span style="font-family: Garamond, serif;font-weight: 700; font-size: 30px; color: #f8f8f8; text-transform: uppercase; letter-spacing: 5px">mirzad </span>
+    <div class="label" style="position: absolute; top: 50px; left: 100px">
+      <span style="font-family: Garamond, serif;font-weight: 700; font-size: 24px; color: white; text-transform: uppercase; letter-spacing: 5px">mirzad</span>
+      <img width="50" height="70"  src="file:///C:/Users/PC/Desktop/1x/7797864.png" alt="">
+      <span style="font-family: Garamond, serif;font-weight: 700; font-size: 24px; color: #f8f8f8; text-transform: uppercase; letter-spacing: 5px">siavash</span>
     </div>
     <div class="avatars">
       <img @click="showCard(0)" class="avatar" src="file:///C:/Users/PC/Desktop/img-1/luffy-profile.png" alt="">
@@ -237,6 +238,17 @@
 
 button:hover{
   background: #ded9d9 !important;
+}
+
+.label::after{
+  content: '';
+  position: absolute;
+  border: 1px solid white;
+  width:  99%;
+  left: 0px;
+  top: 80px;
+
+
 }
 
 
