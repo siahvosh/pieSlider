697dd302721d33a80c84124e6d89b3ebdf6a6abe
siahvosh <siablack370@gmail.com>
2024-02-16 00:11:33 +0330

    set icon

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -38,8 +38,8 @@
     </div>
     <div style="position: absolute; bottom: 160px; left: 28px">
       <button style="width: 400px; height: 50px; border-radius: 5px; background: #eae4e4;">
-        buy now
-        <svg style="transform: translate(5px, 2px)" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2s-.9-2-2-2m10 0c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2s2-.9 2-2s-.9-2-2-2m-1.45-5c.75 0 1.41-.41 1.75-1.03l3.58-6.49A.996.996 0 0 0 20.01 4H5.21l-.94-2H1v2h2l3.6 7.59l-1.35 2.44C4.52 15.37 5.48 17 7 17h12v-2H7l1.1-2zM6.16 6h12.15l-2.76 5H8.53z"/></svg>
+        <span  style="font-weight: 700; font-size: 18px"> buy now </span>
+        <svg style="transform: translate(5px, 4px)" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2s-.9-2-2-2M1 4h2l3.6 7.59L3.62 17H19v-2H7l1.1-2h8.64l4.97-9H5.21l-.94-2H1zm16 14c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2s2-.9 2-2s-.9-2-2-2"/></svg>
       </button>
     </div>
     <div style="position: absolute; top: 150px; left: 200px">
@@ -232,6 +232,10 @@
   align-self: center;
   border: 1px solid grey;
   cursor: pointer;
+}
+
+button:hover{
+  background: #ded9d9 !important;
 }
 
 
